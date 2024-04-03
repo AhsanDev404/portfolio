fd2a363ce5bd5c94c7d03e16fbc4ea3622b35c27
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2024-04-03 11:41:39 +0500

    Update Skill.tsx

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -6,7 +6,7 @@ export default function Skill() {
     <div className="">
       <p className="text-5xl text-center"> My <span className="font-extrabold">Skill</span></p>
        <div className="flex flex-wrap gap-5 my-10 lg:justify-start justify-center">
-       {skills.map((item,index)=><div className={`${index===2 && "bg-gray-900 text-gray-50"} border-2 border-gray-900 w-36 h-36 flex flex-col justify-center items-center rounded-md gap-5`} key={index}><item.icon size={25}/> <p>{item.name}</p> </div>)}
+       {skills.map((item,index)=><div className={`${index===2 && "bg-gray-900 text-gray-50"} border-2 border-gray-900 w-36 h-36 flex flex-col justify-center items-center rounded-md gap-5`} key={index}><item.icon /> <p>{item.name}</p> </div>)}
        </div>
     </div>
   );
